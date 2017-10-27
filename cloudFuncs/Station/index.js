96f7da0a59608b7d32eddeec122856655b3e349c
reluo <21404683@qq.com>
2017-10-27 12:41:39 +0800

    construc

--- cloudFuncs/Station/index.js
+++ cloudFuncs/Station/index.js
@@ -266,7 +266,7 @@ function fetchPartnerByStationId(request, response) {
   query.find().then((sharings)=> {
     var sharingList = []
     sharings.forEach((sharing)=> {
-      sharingList.push(constructProfitSharing(sharing, true, false))
+      sharingList.push(constructProfitSharing(sharing, true, true))
     })
     response.success(sharingList)
   }, (err)=> {
@@ -350,7 +350,7 @@ function fetchInvestorByStationId(request, response) {
       query.find().then((sharings)=> {
         var sharingList = []
         sharings.forEach((sharing)=> {
-          let sharingInfo = constructProfitSharing(sharing, true, false)
+          let sharingInfo = constructProfitSharing(sharing, true, true)
           sharingList.push(sharingInfo)
         })
         response.success(sharingList)
