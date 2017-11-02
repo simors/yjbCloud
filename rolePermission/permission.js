021efb7e17fa9b28756b9b7b6669962d3e8a6172
yang24201 <yang24201@126.com>
2017-11-02 20:18:38 +0800

    add permission

--- rolePermission/permission.js
+++ rolePermission/permission.js
@@ -9,6 +9,8 @@ export const PERMISSION_CODE = {
   DEVICE_EDIT_STATION_ADDR:                   1002,         // 干衣柜综合管理/干衣柜信息管理/编辑干衣柜位置
   DEVICE_CHANGE_STATUS:                       1003,         // 干衣柜综合管理/干衣柜信息管理/修改干衣柜状态
   DEVICE_CHANGE_STATION:                      1004,         // 干衣柜综合管理/干衣柜信息管理/改变所属服务点
+  DEVICE_FETCH_ALL_DEVICE:                    1005,         // 干衣柜综合管理/干衣柜信息管理/获取所有干衣柜
+  DEVICE_FETCH_RELATED_DEVICE:                1006,         // 干衣柜综合管理/干衣柜信息管理/获取所管辖干衣柜
 
   /* 服务点综合管理 */
   STATION_BASE_QUERY:                         2000,         // 服务点综合管理/服务点信息管理/查看基本信息
@@ -16,11 +18,15 @@ export const PERMISSION_CODE = {
   STATION_EDIT:                               2002,         // 服务点综合管理/服务点信息管理/编辑
   STATION_ADD:                                2003,         // 服务点综合管理/服务点信息管理/添加
   STATION_CHANGE_STATUS:                      2004,         // 服务点综合管理/服务点信息管理/修改服务点状态
+  STATION_FETCH_ALL_STATION:                  2005,         // 服务点综合管理/投资人信息管理/获取所有服务点
+  STATION_FETCH_RELATED_STATION:              2006,         // 服务点综合管理/投资人信息管理/获取关联服务点
 
   STATION_INVESTOR_BASE_QUERY:                2100,         // 服务点综合管理/投资人信息管理/查看基本信息
   STATION_INVESTOR_ADD:                       2101,         // 服务点综合管理/投资人信息管理/添加投资人
   STATION_INVESTOR_EDIT:                      2102,         // 服务点综合管理/投资人信息管理/编辑投资人
   STATION_INVESTOR_CHANGE_STATUS:             2103,         // 服务点综合管理/投资人信息管理/修改投资人可用状态
+  STATION_FETCH_ALL_INVESTOR:                 2104,         // 服务点综合管理/投资人信息管理/获取所有投资人
+  STATION_FETCH_RELATED_INVESTOR:             2105,         // 服务点综合管理/投资人信息管理/获取关联服务点投资人
 
   /* 充值与订单管理 */
   RECHARGE_ORDER_QUERY:                       3000,         // 充值与订单管理/订单信息管理/查看
